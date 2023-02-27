77bd2eacf36df423d4741bcee250ea68b87758af
Ahmed8742 <126520773+Ahmed8742@users.noreply.github.com>
2023-02-27 23:14:44 +0200

    Create 3.py

---
+++ 3.py
@@ -0,0 +1,9 @@
+def turtle_3():
+    sc = tr.Screen()
+    cb = tr.Turtle()
+    for i in range(4):
+        cb.forward(30)
+        cb.left(90)
+    cb.forward(30)
+    if __name__ == "__main__":
+        sc.setup(400, 600)
