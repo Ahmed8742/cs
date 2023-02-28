2abb9cf40c598a87c26383d8fb1ced5371c88947
Ahmed8742 <126520773+Ahmed8742@users.noreply.github.com>
2023-02-28 21:07:49 +0200

    Update 3.py

--- 3.py
+++ 3.py
@@ -1,13 +1,32 @@
-import telebot as tb
-import turtle as tr
-import random as rd
-from telebot import types 
-def turtle_3():
-    sc = tr.Screen()
-    cb = tr.Turtle()
+import turtle
+
+sc = turtle.Screen()
+cb = turtle.Turtle()
+
+def draw():
     for i in range(4):
         cb.forward(30)
         cb.left(90)
+
     cb.forward(30)
-    if __name__ == "__main__":
-        sc.setup(400, 600)
+
+if __name__ == "__main__":
+    sc.setup(400, 600)
+    cb.speed(100)
+    for i in range(8):
+        cb.up()
+        cb.setpos(-100, 30 * i)
+        cb.down()
+
+        for j in range(8):
+            if (i + j) % 2 == 0:
+                col = 'black'
+
+            else:
+                col = 'white'
+            cb.fillcolor(col)
+            cb.begin_fill()
+            draw()
+            cb.end_fill()
+    cb.hideturtle()
+    turtle.done()
