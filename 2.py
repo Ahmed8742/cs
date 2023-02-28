e3eab23cb15129779da97d3f8c1576f0ce10a833
Ahmed8742 <126520773+Ahmed8742@users.noreply.github.com>
2023-02-28 21:07:03 +0200

    Update 2.py

--- 2.py
+++ 2.py
@@ -15,25 +15,6 @@ for i in range(200):
     s.pencolor((j,k,l))
 s.penup()
 s.home()
-s.pendown()
-
-m=20
-for i in range(200):
-    s.circle(m+i,45)
-    j=random.random()
-    k=random.random()
-    l=random.random()
-    s.pencolor((j,k,l))
-s.penup()
-s.home()
-s.pendown()
-
-n=30
-for i in range(200):
-    s.circle(n+i,45)
-    j=random.random()
-    k=random.random()
-    l=random.random()
-    s.pencolor((j,k,l))    
+s.pendown()   
 
 turtle.done()
