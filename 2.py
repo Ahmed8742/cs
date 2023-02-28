50678c9f8a95a5baa5194ad4097e3de5de35ad79
Ahmed8742 <126520773+Ahmed8742@users.noreply.github.com>
2023-02-28 21:06:34 +0200

    Update 2.py

--- 2.py
+++ 2.py
@@ -1,37 +1,39 @@
-import telebot as tb
-import turtle as tr
-import random as rd
-from telebot import types 
-def turtle_2():
-    wn=tr.Screen()
-    wn.setup(600,600)
-    wn.bgcolor("white")
-    s=tr.Turtle()
-    r=10
+import turtle
+import random
 
-    for i in range(20):
-        s.circle(r*i)
-        s.penup()
-        s.sety(r*i*-1)
-        s.setx(r*i*-1)
-        s.pendown()
-        j=rd.random()
-        k=rd.random()
-        l=rd.random()
-        s.pencolor((j,k,l))
-    s.penup()
-    s.home()
-    s.pendown()
+wn=turtle.Screen()
+wn.setup(600,600)
+#wn.bgcolor(“white”)
+s=turtle.Turtle()
 
-    for i in range(20):
-        s.circle(r*i)
-        s.penup()
-        s.setx(r*i)
-        s.sety(r*i*-1)
-        s.pendown()
-        j=rd.random()
-        k=rd.random()
-        l=rd.random()
-        s.pencolor((j,k,l))
-    s.hideturtle()
-    tr.done()
+r=10
+for i in range(200):
+    s.circle(r+i,45)
+    j=random.random()
+    k=random.random()
+    l=random.random()
+    s.pencolor((j,k,l))
+s.penup()
+s.home()
+s.pendown()
+
+m=20
+for i in range(200):
+    s.circle(m+i,45)
+    j=random.random()
+    k=random.random()
+    l=random.random()
+    s.pencolor((j,k,l))
+s.penup()
+s.home()
+s.pendown()
+
+n=30
+for i in range(200):
+    s.circle(n+i,45)
+    j=random.random()
+    k=random.random()
+    l=random.random()
+    s.pencolor((j,k,l))    
+
+turtle.done()
